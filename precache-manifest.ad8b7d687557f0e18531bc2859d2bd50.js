d5a5715e46374067c83be27decd2a7b51b30a625
AlexanderPodvoyski <alexander.podvoyski@upsilonit.com>
2020-07-26 17:34:20 +0300

    Updates

---
+++ precache-manifest.ad8b7d687557f0e18531bc2859d2bd50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec061e11b6eb594ea72a704390d80489",
+    "url": "/steam-board/index.html"
+  },
+  {
+    "revision": "ae73b317312f8a82a6b5",
+    "url": "/steam-board/static/css/main.f128f961.chunk.css"
+  },
+  {
+    "revision": "280125a832c7e0505505",
+    "url": "/steam-board/static/js/2.3879c600.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/steam-board/static/js/2.3879c600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae73b317312f8a82a6b5",
+    "url": "/steam-board/static/js/main.406943bc.chunk.js"
+  },
+  {
+    "revision": "6fcfda050aa62b6e37b3",
+    "url": "/steam-board/static/js/runtime-main.09fa8190.js"
+  }
+]);
